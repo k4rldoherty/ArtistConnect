cd5e9b197df92cfeee7b68b26ccfc425e75623df
Karl Doherty <karl.doherty27@mail.dcu.ie>
2022-10-06 19:28:51 +0100

    Working on connecting firebase database to app

--- src/ArtistConnect/src/app/app.module.ts
+++ src/ArtistConnect/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// Firebase Installed - need to initialize app
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
