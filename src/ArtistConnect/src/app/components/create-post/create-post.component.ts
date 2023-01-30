736665c0917aaeb73c79213ccd2dc77b210bb815
yury chupahin <yury.chupahin2@mail.dcu.ie>
2023-01-30 00:13:45 +0000

    create a document in database for each post

--- src/ArtistConnect/src/app/components/create-post/create-post.component.ts
+++ src/ArtistConnect/src/app/components/create-post/create-post.component.ts
@@ -1,27 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatInput } from '@angular/material/input';
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.scss']
 })
-export class CreatePostComponent implements OnInit {
+export class CreatePostComponent implements OnInit{
+  artist: string;
+  songName: string;
+  songUrl: string;
+  eventName: string;
+  eventUrl: string;
 
-  selectedOption = 'option1';
-
-  constructor(private afAuth: AngularFireAuth) { }
+  selectedOption = 'song';
+  constructor(private firestore: AngularFirestore, private afAuth: AngularFireAuth) {
+    this.artist = '';
+    this.songName = '';
+    this.songUrl = '';
+    this.eventName = '';
+    this.eventUrl = '';
+    }
 
   ngOnInit(): void {
+
   }
 
   onPost() {
     this.afAuth.authState.subscribe(user => {
       if (user) {
-        console.log(user.uid);
+        if (this.selectedOption == 'song'){
+          console.log(user.uid, this.songName, this.artist, this.songUrl);
+          this.firestore.collection('posts').add({
+            type: this.selectedOption,
+            uid: user.uid,
+            songName: this.songName,
+            artist: this.artist,
+            songUrl: this.songUrl
+          });
+        }
+        else {
+          console.log(user.uid, this.eventName, this.eventUrl);
+          this.firestore.collection('posts').add({
+            type: this.selectedOption,
+            uid: user.uid,
+            eventName: this.eventName,
+            eventUrl: this.eventUrl
+          });
+        }
       }
     });
   }
-
-
 }
